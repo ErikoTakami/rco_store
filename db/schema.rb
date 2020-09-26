d258f8d10bf738855ae06ce3d50ca33a7524b513
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-26 12:44:26 +0900

    Usersテーブルのカラム修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_112346) do
+ActiveRecord::Schema.define(version: 2020_09_26_032743) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "name_kana", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.boolean "admin", default: false, null: false
@@ -23,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_112346) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
