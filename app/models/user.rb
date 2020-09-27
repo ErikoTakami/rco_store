4de6c690762047221a3780414fb7fa649263f09f
erikotakami <tik.mtkneu_217@ezweb.ne.jp>
2020-09-27 23:14:18 +0900

    userモデルとaddressモデルのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, :name_kana, :admin, presence: true
